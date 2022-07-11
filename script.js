63cdac9b4d3bb611f45d7d48cadc3b33312da9f9
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-07-11 15:39:21 +0200

    aggiunta nome corretto al documento e bootstrap

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 const user = new Vue({
     el : `#name`,
+    name : `hello Vue`,
     data: {
         firstName : `Simone`,
+        lastName : `Di Prima`,
         url :`https://www.teamworld.it/wp-content/uploads/2022/04/Clark-su-Netflix.jpg`,
     },
 })
