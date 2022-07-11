b5aefbb5adf16f12420aa839723bba38a956099d
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-07-11 15:10:18 +0200

    esercizio versione bonus

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const user = new Vue({
     el : `#name`,
     data: {
         firstName : `Simone`,
+        url :`https://www.teamworld.it/wp-content/uploads/2022/04/Clark-su-Netflix.jpg`,
     },
 })
